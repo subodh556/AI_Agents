effe9136fc64a2ec65d6b7235d11fcf163db6927
Subodh <sohonisubodh@gmail.com>
2025-02-04 09:55:40 +0530

    Agent using langgraph

--- 03_Agent_langraph.py
+++ 03_Agent_langraph.py
@@ -82,3 +82,15 @@ display(
     )
 )
 
+#Testing the pipeline
+sample_text = """
+    OpenAI has announced the GPT-4 model, which is a large multimodal model that exhibits human-level performance on various professional benchmarks. It is developed to improve the alignment and safety of AI systems.
+additionally, the model is designed to be more efficient and scalable than its predecessor, GPT-3. The GPT-4 model is expected to be released in the coming months and will be available to the public for research and development purposes.
+"""
+
+state_input = {"text": sample_text}
+result = app.invoke(state_input)
+
+print("Classification:", result["classification"])
+print("\nEntities:", result["entities"])
+print("\nSummary:", result["summary"])
